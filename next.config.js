a3b18671f335b37bbd0ccadb4c0b7d30d2fda2e7
vedatkokmen <vedatkoekmen@gmail.com>
2022-03-20 17:40:49 +0300

    next image fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.pexels.com"],
-  },
-  hostname: "images.pexels.com",
+	reactStrictMode: true,
+	images: {
+		loader: "akamai",
+		path: "",
+	},
 };
